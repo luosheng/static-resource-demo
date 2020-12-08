9d782704475c7eb937147ec884d2c14c58ca97f4
LUO Sheng <luosheng1986@gmail.com>
2020-12-08 11:25:37 +0800

    Change OpenAPI info title to Chinese

--- src/main/java/net/luosheng/Application.java
+++ src/main/java/net/luosheng/Application.java
@@ -4,12 +4,7 @@ import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.info.*;
 
-@OpenAPIDefinition(
-    info = @Info(
-            title = "static-resource-demo",
-            version = "0.0"
-    )
-)
+@OpenAPIDefinition(info = @Info(title = "测试", version = "0.0"))
 public class Application {
 
     public static void main(String[] args) {
